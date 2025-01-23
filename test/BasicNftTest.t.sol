066434cd9fb44e95357c973e64ecd3543ce6c38e
Garba Jamiu <garbajamiu2000@gmail.com>
2025-01-23 11:22:14 +0100

    deploy script and test nft functions

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "script/DeployBasicNft.s.sol";
+import {BasicNft} from "src/BasicNft.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER = makeAddr("user");
+
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public {
+        string memory actualName = basicNft.name();
+        string memory expectedName = "Dogie";
+
+        // Can not compare 2 strings because strings are array of bytes , so we have to convert them to
+        // keccak256 hashes , then compare the hashes
+
+        assert(
+            keccak256(abi.encodePacked(actualName)) ==
+                keccak256(abi.encodePacked(expectedName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(PUG)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
+    }
+}
